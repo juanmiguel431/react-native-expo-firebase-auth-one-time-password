8119224e5e7f0669181bb53f87caa537ee084226
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-09 21:19:24 -0400

    Safe Area Provider added.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto"/>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
